0e936a47fe271c0c19d9d978e1505c36e4e7b857
bee-p <weileenw@naver.com>
2024-01-19 20:42:19 +0900

    fix: 응답 http 내용이 올바르게 뜨지 않던 문제 수정
    - Exception 발생 방식 변경
    - 해당하는 상태에 맞게 http status code를 전달하도록 함

--- server/src/main/java/com/oops/server/service/UserService.java
+++ server/src/main/java/com/oops/server/service/UserService.java
@@ -6,7 +6,9 @@ import com.oops.server.dto.user.SignUpRequest;
 import com.oops.server.entity.User;
 import com.oops.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class UserService {
@@ -29,14 +31,14 @@ public class UserService {
     // 중복 회원 검증
     public void validateDuplicateUser(String email, String snsType) {
         if (userRepository.findByEmailAndSnsType(email, snsType) != null) {
-            throw new RuntimeException(DUPLICATE_USER.get());
+            throw new ResponseStatusException(HttpStatus.CONFLICT, DUPLICATE_USER.get());
         }
     }
 
     // 중복 닉네임 검증
     public void validateDuplicateName(String name) {
         if (userRepository.findByName(name) != null) {
-            throw new RuntimeException(DUPLICATE_NAME.get());
+            throw new ResponseStatusException(HttpStatus.CONFLICT, DUPLICATE_NAME.get());
         }
     }
 }
