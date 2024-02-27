bbc3287060f40ddc390c653502d356df579763e7
bee-p <weileenw@naver.com>
2024-02-28 02:15:58 +0900

    feat: DateTodo DB 테이블 구조 수정
    - 할 일에 고유 아이디값 부여
    - 완료 여부 체크 필드 추가

--- server/src/main/java/com/oops/server/entity/DateTodo.java
+++ server/src/main/java/com/oops/server/entity/DateTodo.java
@@ -1,10 +1,10 @@
 package com.oops.server.entity;
 
-import com.oops.server.compositekey.DateTodoID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
@@ -14,17 +14,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@IdClass(DateTodoID.class)
 @Table(name = "date_todo")
 public class DateTodo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id")
+    private Long todoId;
+
     @ManyToOne
     @JoinColumns({
             @JoinColumn(name = "date", referencedColumnName = "date"),
@@ -32,9 +36,12 @@ public class DateTodo {
     })
     private Schedule schedule;
 
-    @Id
     @Column(length = 20)
     private String content;
+
+    @ColumnDefault("false")
+    @Column(name = "is_complete")
+    private boolean isComplete;
 
     public static DateTodo create(Schedule schedule, String content) {
         return DateTodo.builder()
