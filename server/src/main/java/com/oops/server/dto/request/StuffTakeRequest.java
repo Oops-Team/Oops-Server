d5b45257641b4e144e90fd68007eec4145a1ef3c
bee-p <weileenw@naver.com>
2024-03-07 01:45:51 +0900

    style: 소지품 챙김 request dto 필드 순서 변경
    - 챙겨야 할 것 수정(소지품 삭제) API에서 동일한 Request DTO로 재사용하기 위함

--- server/src/main/java/com/oops/server/dto/request/StuffTakeRequest.java
+++ server/src/main/java/com/oops/server/dto/request/StuffTakeRequest.java
@@ -3,6 +3,6 @@ package com.oops.server.dto.request;
 import java.time.LocalDate;
 
 public record StuffTakeRequest(
-        String stuffName,
-        LocalDate date
+        LocalDate date,
+        String stuffName
 ) {}
